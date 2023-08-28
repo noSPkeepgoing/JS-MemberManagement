ce204b21861ad400e00c9eee59cc84a6c21c0a8c
sunpaaaa <sunpaaaa@gmail.com>
2023-08-28 14:46:10 +0900

    Refactor CardItem

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,13 +1,12 @@
 import Modal from './Modal.js';
 import { deleteData, setData } from '../firebase/data.js';
-import CardList from './CardList.js';
 import { cardList } from '../main.js';
 
 export default class CardItem {
   constructor(info = {}) {
     this.el = document.createElement('li');
-    this.id = info.id;
     this.info = {
+      id: info.id,
       photo: info.data.photo,
       name: info.data.name,
       email: info.data.email,
@@ -17,14 +16,13 @@ export default class CardItem {
     this.render();
   }
   render() {
-    const { photo, name, email, phone, department } = this.info;
+    const { id, photo, name, email, phone, department } = this.info;
 
     this.el.classList.add('card');
     this.el.innerHTML = `
         <p class="card_photo"><img src=${
-          photo
-            ? photo
-            : 'https://firebasestorage.googleapis.com/v0/b/wanna-go-home-9ebdc.appspot.com/o/empty_user.png?alt=media&token=507c8fc6-6407-4c4f-8627-fe33ed36539f'
+          photo ||
+          'https://firebasestorage.googleapis.com/v0/b/wanna-go-home-9ebdc.appspot.com/o/empty_user.png?alt=media&token=507c8fc6-6407-4c4f-8627-fe33ed36539f'
         } /></p>
         <div class="card_info">
             <p class="info_name">${name}</p>
@@ -35,9 +33,8 @@ export default class CardItem {
     `;
 
     this.el.addEventListener('click', () => {
-      const body = document.querySelector('body');
-      const modal = new Modal(this.info, 'update', this.id);
-      body.append(modal.el);
+      const modal = new Modal(this.info, 'update', id);
+      document.body.append(modal.el);
     });
 
     // 카드 삭제
@@ -50,21 +47,14 @@ export default class CardItem {
 
     deleteBtnEl.addEventListener('click', async (e) => {
       e.stopPropagation();
-      if (confirm(`${name}을 정말 삭제하시겠어요?`)) {
-        deleteData(this.id);
-        const newData = await setNewData();
-        cardList.update(newData);
+      if (!confirm(`${name}을 정말 삭제하시겠어요?`)) {
+        return;
       }
+      await deleteData(id);
+      const newData = await setData();
+      cardList.update(newData);
     });
 
     this.el.append(deleteBtnEl);
   }
 }
-
-const setNewData = async () => {
-  const newData = [];
-  await setData().then((res) => {
-    res.forEach((el) => newData.push(el));
-  });
-  return newData;
-};
